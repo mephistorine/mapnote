b26281029922ccf6c044e71c5ec2e1de050f41b2
Sam Bulatov <mephistorine@gmail.com>
2022-04-09 00:55:57 +0300

    feat: Реализован основной функционал редактора Markdown

--- src/app/ui/place-edit-dialog/place-edit-dialog.module.ts
+++ src/app/ui/place-edit-dialog/place-edit-dialog.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from "@angular/forms"
+import { MdEditorModule } from "../shared/components/md-editor/md-editor.module"
 import { RatingModule } from "../shared/components/rating/rating.module"
 import { TagsModule } from "../shared/components/tags/tags.module"
 import { PlaceEditComponent } from './components/place-edit/place-edit.component';
@@ -16,7 +17,8 @@ import { PlaceEditComponent } from './components/place-edit/place-edit.component
     CommonModule,
     RatingModule,
     ReactiveFormsModule,
-    TagsModule
+    TagsModule,
+    MdEditorModule
   ]
 })
 export class PlaceEditDialogModule { }
