fe4db8df7fb53741a74c94d2ec7d91b2fb977518
Sam Bulatov <mephistorine@gmail.com>
2022-04-11 19:36:10 +0300

    feat: Добавлен компонент редактирования тегов

--- src/app/ui/shared/components/tags/tags.component.ts
+++ src/app/ui/shared/components/tags/tags.component.ts
@@ -11,10 +11,16 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms"
       useExisting: forwardRef(() => TagsComponent),
       multi: true
     }
-  ]
+  ],
+  host: {
+    "[class.is-disabled]": "isDisabled",
+    "(click)": "onClickAddButton()"
+  }
 })
 export class TagsComponent implements OnInit, ControlValueAccessor {
   public tags: string[] = []
+  public newTagNameValue: string | null = null
+  public isDisabled: boolean = false
 
   constructor() {
   }
@@ -22,16 +28,56 @@ export class TagsComponent implements OnInit, ControlValueAccessor {
   public ngOnInit(): void {
   }
 
+  public onChange(tags: string[]): void {
+  }
+
+  public onTouch(): void {
+  }
+
   public registerOnChange(fn: (tags: string[]) => void): void {
+    this.onChange = fn
   }
 
   public registerOnTouched(fn: () => void): void {
+    this.onTouch = fn
   }
 
   public setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled
   }
 
-  public writeValue(tags: string[]): void {
+  public writeValue(tags: string[] | null): void {
+    if (tags === null) {
+      this.tags = []
+      return
+    }
+
     this.tags = tags
+  }
+
+  public onClickCloseButton(event: MouseEvent, tagNameToRemove: string): void {
+    event.stopPropagation()
+    this.tags = this.tags.filter((tagName) => tagName !== tagNameToRemove)
+    this.onChange(this.tags)
+    this.onTouch()
+  }
+
+  public onClickAddButton(): void {
+    if (this.newTagNameValue === null || (typeof this.newTagNameValue === "string" && this.newTagNameValue.length <= 0)) {
+      return
+    }
+
+    this.tags = this.tags.concat(this.newTagNameValue)
+    this.newTagNameValue = null
+    this.onChange(this.tags)
+    this.onTouch()
+  }
+
+  public onKeyUpEnter(event: KeyboardEvent): void {
+    if (event.key !== "Enter") {
+      return
+    }
+
+    this.onClickAddButton()
   }
 }
--- src/app/ui/shared/components/tags/tags.module.ts
+++ src/app/ui/shared/components/tags/tags.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from "@angular/core"
 import { CommonModule } from "@angular/common"
+import { FormsModule } from "@angular/forms"
+import { IconModule } from "../icon/icon.module"
 import { TagsComponent } from "./tags.component"
 
 @NgModule({
@@ -10,7 +12,9 @@ import { TagsComponent } from "./tags.component"
     TagsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    IconModule,
+    FormsModule
   ]
 })
 export class TagsModule {
