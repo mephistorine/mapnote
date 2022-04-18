a7f41d3bc916568b0dbc52d79cb8b512082b3685
DevidzeIL <devidzeil@icloud.com>
2022-04-19 01:52:37 +0400

    homework_9

---
+++ src/app/ui/shared/components/md-editor/md-editor.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from "@angular/core"
+import { ControlValueAccessor } from "@angular/forms"
+import { DomSanitizer } from "@angular/platform-browser"
+import snarkdown from "snarkdown"
+
+type EditorTabName = "RAW" | "RESULT"
+
+@Component({
+  selector: "mn-md-editor",
+  templateUrl: "./md-editor.component.html",
+  styleUrls: [ "./md-editor.component.scss" ]
+})
+export class MdEditorComponent implements OnInit, ControlValueAccessor {
+  public selectedTabName: EditorTabName = "RAW"
+  public editorValue: string = ''
+  public isDisabled: boolean = false
+
+  constructor(private domSanitizer: DomSanitizer) {
+  }
+
+  public ngOnInit(): void {
+  }
+
+  public onClickTabButton(tabName: EditorTabName) {
+    this.selectedTabName = tabName
+  }
+
+  public convertMarkdownToHtml(markdown: string) {
+    return this.domSanitizer.bypassSecurityTrustHtml(snarkdown(markdown))
+  }
+
+  public registerOnChange(fn: any): void {
+  }
+
+  public registerOnTouched(fn: any): void {
+  }
+
+  public setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled
+  }
+
+  public writeValue(markdown: string): void {
+    this.editorValue = markdown
+  }
+}
